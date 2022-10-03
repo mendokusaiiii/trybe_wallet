fb5e8cf2c3e9b88b35107e24037599d0d38fb053
mendokusaiiii <victor_cabral_o@hotmail.com>
2022-10-03 15:19:08 -0300

    configurnado o redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,15 @@
-// Coloque aqui suas actions
+const INITIAL_STATE = {
+  currencies: [],
+  allValueBRL: 0,
+  editor: false,
+  idToEdit: 0,
+};
+
+wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default wallet;
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,20 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SUBMIT_EMAIL } from '../actions/index';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const stateFolder = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SUBMIT_EMAIL: {
+    return {
+      ...state,
+      email: action.payload.email,
+    };
+  }
+  default:
+    return state;
+  }
+};
+
+export default stateFolder;
